73aabb15e92daaea4dec182fc076dfe8e3e2181f
Mucahit Senol <mucahitsenol86@gmail.com>
2013-12-01 01:51:41 +0200

    heroku 4

--- app.rb
+++ app.rb
@@ -6,8 +6,18 @@ require 'json/ext'
 
 include Mongo
 
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
+
+
 configure do
-  conn = MongoClient.new("localhost", 27017)
+  conn = get_connection
   set :mongo_connection, conn
   set :mongo_db, conn.db('chass')
 end
